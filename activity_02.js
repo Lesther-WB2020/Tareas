0636cab5d190fc26791dd680c8e6fc86c9bba0b6
Lesther Xitumul Manuel <lesthermanuel6@gmail.com>
2020-02-06 22:29:18 -0600

    AGREGANDO LA ACTIVIDAD 02 A GIT

---
+++ activity_02.js
@@ -0,0 +1,32 @@
+//ésta es la actividad dos :)
+var mes = 10;
+var mes_evaluado = false;
+
+if((mes>=1)&&(mes<=2)){
+
+    document.write("ES INVIERNO"); mes_evaluado = true;
+
+    }else if((mes>=3)&&(mes<=5)){
+
+    document.write("ES PRIMAVERA"); mes_evaluado = true;
+
+       }else if((mes>=6)&&(mes<=8)){
+
+       document.write("ES VERANO"); mes_evaluado = true;
+
+          }else if((mes>=9)&&(mes<=11)){
+
+          document.write("ES OTOÑO"); mes_evaluado = true;
+
+             }else if(mes == 12){
+
+             document.write("ES INVIERNO"); mes_evaluado = true;
+    
+}
+
+if(mes_evaluado==true){
+
+    document.write(" //No. DE CARNÉ: 20000742");
+    document.write(" //LESTHER CARLOS HORALDO XITUMUL MANUEL");
+
+}
